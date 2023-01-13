d0a27d92d80e6f25ca0b10f9c4ab84ba75728b4c
yeobi01 <yeobi_01@naver.com>
2023-01-14 01:40:53 +0900

    C++

---
+++ 3장/async.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+fs.readFile('./readme.txt', (err, data) => {
+    if(err) throw err;
+    console.log('1번', data.toString());
+});
+fs.readFile('./readme.txt', (err, data) => {
+    if(err) throw err;
+    console.log('2번', data.toString());
+});
+fs.readFile('./readme.txt', (err, data) => {
+    if(err) throw err;
+    console.log('3번', data.toString());
+});
+fs.readFile('./readme.txt', (err, data) => {
+    if(err) throw err;
+    console.log('4번', data.toString());
+});

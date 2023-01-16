37db08770366d61e6b67fc351d75aebce1df26c4
yeobi01 <yeobi_01@naver.com>
2023-01-16 17:30:01 +0900

    4장

---
+++ 4장/createServer.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const server = http.createServer(async (req, res) => {
+    try{
+        res.writeHead(200, { 'Content-Type': 'text/html; charset-utf-8' });
+        const data = await fs.readFile('./server.html');
+        res.end(data); 
+    } catch(error){
+        console.error(err);
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset-utf-8' });
+        res.end(err.message);
+    }
+})
+    .listen(8080);
+server.on('listning', () => {
+    console.log('8080번 포트에서 서버 대기 중입니다.');
+});

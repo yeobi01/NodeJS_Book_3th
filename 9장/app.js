c44c41d57c949abb6b0f00c8a2ca4c0b11293d41
yeobi01 <yeobi_01@naver.com>
2023-01-26 20:50:23 +0900

    9장

--- 9장/app.js
+++ 9장/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const { sequelize } = require('./models');
 
 dotenv.config(); // process.env
 const pageRouter = require('./routes/page');
@@ -16,9 +17,16 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
+sequelize.sync()
+    .then(() => {
+        console.log('데이터베이스 연결 성공')
+    })
+    .catch((err) => {
+        console.error(err);
+    })
 
 app.use(morgan('dev'));
-app.use(express.static(path.josin(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
--- 9장/models/index.js
+++ 9장/models/index.js
@@ -1,43 +1,29 @@
-'use strict';
-
+const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config')[env];
 const db = {};
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+db.sequelize = sequelize;
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
+const basename = path.basename(__filename);
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file));
+    console.log(file, model.name);
     db[model.name] = model;
+    model.initiate(sequelize);
   });
-
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
+  if(db[modelName].associate){
     db[modelName].associate(db);
   }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+})
 
 module.exports = db;

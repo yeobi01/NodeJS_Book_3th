031dd4103633fddc0b1b796daa49ecefe7d3d803
yeobi01 <yeobi_01@naver.com>
2023-02-08 23:52:46 +0900

    CORS 에러 해결하기

--- 10장/middlewares/index.js
+++ 10장/middlewares/index.js
@@ -1,4 +1,7 @@
 const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const { Domain } = require('../models');
 
 exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()) { // 패스포트 통해서 로그인 했는지
@@ -32,5 +35,43 @@ exports.verifyToken = (req, res, next) => {
             code: 401,
             message: '유효하지 않은 토큰입니다.',
         })
+    }
+}
+
+exports.apiLimiter = async (req, res, next) => {
+    /*let user;
+    if(req.locals.decode.id){
+        user = await User.findOne({ where: { id: req.locals.decode.id } });
+    }*/
+    rateLimit({
+        windowMs: 60 * 1000,
+        max: 10, //user?.type === 'premium' ? 1000 : 10,
+        handler(req, res){
+            res.status(this.statusCode).json({
+                code: this.statusCode,
+                message: '1분에 열 번만 요청할 수 있습니다'
+            });
+        }
+    })(req, res, next);
+}
+
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: '새로운 버전 나왔습니다. 새로운 버전 사용하세요',
+    });
+}
+
+exports.corsWhenDomainMatches = async(req, res, next) => {
+    const domain = await Domain.findOne({
+        where: { host: new URL(req.get('origin')).host }
+    })
+    if(domain){
+        cors({
+            origin: req.get('origin'),
+            credentials: true,
+        })(req, res, next);
+    } else{
+        next();
     }
 }
